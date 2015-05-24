856791c3f9bfa2d22f86468c7b71353dae236a07
Gabriel Franck <gabfranck.pro@gmail.com>
2015-05-24 03:39:30 +0200

    Feature : Colors for playing games list

--- php/connect_db.php
+++ php/connect_db.php
@@ -1,8 +1,8 @@
-<?php	
+<?php
  	$db_name  = 'Kwizybo';
     $hostname = '127.0.0.1';
     $username_db = 'root';
-    $password_db = 'gab666cool';
-    $dbh = new PDO("mysql:host=$hostname;dbname=$db_name", $username_db, $password_db);
+    $password_db = 'root';
+    $dbh = new PDO("mysql:host=$hostname;dbname=$db_name;port=8889", $username_db, $password_db);
     return $dbh
 ?>
